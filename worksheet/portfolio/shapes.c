c9b6b82e144bdb4cb4135f9db7e518365c1b1a56
SawyerCowley <xmgj0858@leeds.ac.uk>
2026-03-27 09:46:23 +0000

    worksheet started

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -10,6 +10,22 @@ Point makePoint( float x, float y ) {
     return new;
 }
 
+Line makeLine( Point p1, Point p2){
+    return new;
+}
+
+Triangle makeTriangle( Point p1, Point p2, Point p3){
+    Line(p1, p2);
+    Line(p2, p3);
+    Line(p1, p3);
+    return new;
+}
+
+lineLength( Line l){
+    float temp = ((l.x2 - l.x1)**) + ((l.y2 - l.y1)**);
+    
+}
+
 // complete other functions below
 // - start with stubs as above
 // - compile regularly to test syntax
